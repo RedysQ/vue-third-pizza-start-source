fe9774ad7b6a8f1465ecc652d424155dd43128a1
redysq <hamitcher@gmail.com>
2025-03-20 18:55:06 +0300

    Модули 2 и 3

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,13 @@
 <template>
   <app-layout>
-    <home-view />
+    <router-view />
   </app-layout>
 </template>
 
 <script setup>
 import AppLayout from "@/layouts/AppLayout.vue";
-import HomeView from "@/views/HomeView.vue";
 </script>
+
+<style lang="scss">
+@import "@/assets/scss/app.scss";
+</style>
